190fc5fed9a0d0e345c52aa27abd0f525fd20bfa
Steli666 <stela.kaleva@abv.bg>
2024-04-20 21:55:01 +0300

    New Login Interface

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -10,7 +10,7 @@ class LoginScreen extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/icons/bg2.jpg'),
+            image: AssetImage('assets/icons/bg9.jpg'),
             fit: BoxFit.cover, // Adjust as needed
           ),
         ),
@@ -25,7 +25,7 @@ class LoginScreen extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 47.0, // Adjust font size as needed
                     fontWeight: FontWeight.bold, // Adjust font weight as needed
-                    color: Colors.pink.shade100, // Adjust color as needed
+                    color: Colors.red.shade100, // Adjust color as needed
                   ),
                 ),SizedBox(height: 36),
 
@@ -34,7 +34,7 @@ class LoginScreen extends StatelessWidget {
                   child: TextField(
                     decoration: InputDecoration(
                       filled: true,
-                      fillColor: Colors.deepPurple.shade200,
+                      fillColor: Colors.red.shade50,
                       labelText: 'Email',
                       hintText: 'Enter your email',
                       border: OutlineInputBorder(),
@@ -48,7 +48,7 @@ class LoginScreen extends StatelessWidget {
                     obscureText: true,
                     decoration: InputDecoration(
                       filled: true,
-                      fillColor: Colors.deepPurple.shade200,
+                      fillColor: Colors.red.shade50,
                       labelText: 'Password',
                       hintText: 'Enter your password',
                       border: OutlineInputBorder(),
@@ -65,7 +65,7 @@ class LoginScreen extends StatelessWidget {
                     },
                     style: ElevatedButton.styleFrom(
                       foregroundColor: Colors.white,
-                      backgroundColor: Colors.deepPurple.shade400,
+                      backgroundColor: Colors.deepPurple.shade700,
                       shadowColor: Colors.purple, // Set shadow color
                       elevation: 5, // Set elevation (shadow intensity)
                     ),
