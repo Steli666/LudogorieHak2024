ab3100c9222c017f61fa533f4712dcac555ef845
Steli666 <stela.kaleva@abv.bg>
2024-04-20 20:44:52 +0300

    Login Interface

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -6,41 +6,80 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login with Email and Password"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter your email',
-                border: OutlineInputBorder(),
-              ),
+
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/icons/bg2.jpg'),
+            fit: BoxFit.cover, // Adjust as needed
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Login', // Title text
+                  style: TextStyle(
+                    fontSize: 47.0, // Adjust font size as needed
+                    fontWeight: FontWeight.bold, // Adjust font weight as needed
+                    color: Colors.pink.shade100, // Adjust color as needed
+                  ),
+                ),SizedBox(height: 36),
+
+                Container(
+                  height: 50.0, // Adjust height here
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.deepPurple.shade200,
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  height: 50.0, // Same height as email field
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.deepPurple.shade200,
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 36),
+                SizedBox(
+                  width: 150.0,
+                  height: 47.0, // Adjust button height
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Add login functionality here
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.deepPurple.shade400,
+                      shadowColor: Colors.purple, // Set shadow color
+                      elevation: 5, // Set elevation (shadow intensity)
+                    ),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 16),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Add login functionality here
-              },
-              child: Text('Login'),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
-
